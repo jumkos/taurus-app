56f9dc66062f82987e383dd12181fed526427843
Kris Hendrawan A.S <hendrazxc@gmail.com>
2023-11-17 09:58:09 +0700

    perbaikan report

--- app/Exports/ReferralReportExport.php
+++ app/Exports/ReferralReportExport.php
@@ -3,6 +3,7 @@
 namespace App\Exports;
 
 use App\Models\User;
+use DateTimeImmutable;
 use Illuminate\Database\Query\Builder;
 use Illuminate\Support\Facades\DB;
 use Maatwebsite\Excel\Concerns\Exportable;
@@ -10,19 +11,22 @@ use Maatwebsite\Excel\Concerns\FromCollection;
 use Maatwebsite\Excel\Concerns\ShouldAutoSize;
 use Maatwebsite\Excel\Concerns\WithCustomStartCell;
 use Maatwebsite\Excel\Concerns\WithDefaultStyles;
+use Maatwebsite\Excel\Concerns\WithColumnFormatting;
 use Maatwebsite\Excel\Concerns\WithEvents;
 use Maatwebsite\Excel\Concerns\WithHeadings;
 use Maatwebsite\Excel\Concerns\WithStyles;
 use Maatwebsite\Excel\Events\AfterSheet;
 use Maatwebsite\Excel\Sheet;
+use PhpOffice\PhpSpreadsheet\Shared\Date;
 use PhpOffice\PhpSpreadsheet\Style\Alignment;
 use PhpOffice\PhpSpreadsheet\Style\Border;
 use PhpOffice\PhpSpreadsheet\Style\Fill;
 use PhpOffice\PhpSpreadsheet\Style\Color;
+use PhpOffice\PhpSpreadsheet\Style\NumberFormat;
 use PhpOffice\PhpSpreadsheet\Style\Style;
 use PhpOffice\PhpSpreadsheet\Worksheet\Worksheet;
 
-class ReferralReportExport implements FromCollection, WithHeadings, ShouldAutoSize, WithStyles, WithDefaultStyles, WithCustomStartCell, WithEvents
+class ReferralReportExport implements FromCollection, WithHeadings, ShouldAutoSize, WithStyles, WithDefaultStyles, WithCustomStartCell, WithEvents, WithColumnFormatting
 {
     /**
     * @return \Illuminate\Support\Collection
@@ -45,8 +49,8 @@ class ReferralReportExport implements FromCollection, WithHeadings, ShouldAutoSi
                 /** @var Sheet $sheet */
                 $sheet = $event->sheet;
 
-                $sheet->mergeCells('A1:G1');
-                $sheet->setCellValue('A1', "Referral Report ".$this->user_name);
+                $sheet->mergeCells('A1:I1');
+                $sheet->setCellValue('A1', "Referral Report ".$this->user_name." as of ".date('d M Y H:i:s'));
 
                 $styleArray = [
                     'alignment' => [
@@ -54,7 +58,7 @@ class ReferralReportExport implements FromCollection, WithHeadings, ShouldAutoSi
                     ],
                 ];
 
-                $cellRange = 'A1:G1'; // All headers
+                $cellRange = 'A1:I1'; // All headers
                 $event->sheet->getDelegate()->getStyle($cellRange)->applyFromArray($styleArray);
             },
         ];
@@ -67,7 +71,7 @@ class ReferralReportExport implements FromCollection, WithHeadings, ShouldAutoSi
 
     public function headings(): array
     {
-        return ["Referral Code", "Customer Name", "Product Category", "Status", "Approved Limit", "Poin", "Marketing Name"];
+        return ["Referral Code", "Offering Date", "Customer Name", "Product Category", "Status", "Last Update","Approved Limit", "Poin", "Marketing Name"];
     }
 
     public function styles(Worksheet $sheet)
@@ -87,7 +91,7 @@ class ReferralReportExport implements FromCollection, WithHeadings, ShouldAutoSi
             ],],
 
             // // Styling a specific cell by coordinate.
-            'A1:G'.$this->row => [
+            'A1:I'.$this->row => [
                 'borders' => [
                     'allBorders' => [
                         'borderStyle' => Border::BORDER_THIN,
@@ -108,6 +112,14 @@ class ReferralReportExport implements FromCollection, WithHeadings, ShouldAutoSi
         ];
     }
 
+    public function columnFormats(): array
+    {
+        return [
+            'B' => "dd-mm-yyyy",
+            'F' => "dd-mm-yyyy",
+        ];
+    }
+
     public function collection()
     {
         $report = DB::table('referrals as r')
@@ -124,22 +136,29 @@ class ReferralReportExport implements FromCollection, WithHeadings, ShouldAutoSi
                         ->join('product_categories as pc','pc.id', '=', 'r.product_category_id')
                         ->join('user_details as ud','ud.id','=','r.refer_id')
                         ->where(function (Builder $query) {
-                            $query->where('r.issuer_id','=', $this->user_id)
+                            $query->where(function (Builder $query) {
+                                $query->where('r.issuer_id','=', $this->user_id);
+                                    //   ->where('rs.STATUS_ID','>',4);
+                                    //   ->orWhere(0, '=', $this->user_id);
+                            })
                                   ->orWhereRaw('0 = ?', $this->user_id);
                                 //   ->orWhere(0, '=', $this->user_id);
                         })
-                        ->where('rs.STATUS_ID','>',4)
-                        ->select('r.id as id', 'r.cust_name','pc.name as product_category','sp.name as status', 'r.approved_nominal as approved_limit', 'ud.name as marketing_name')
+
+                        // ->select('r.id as id', 'r.offering_date as offering_date', 'r.cust_name','pc.name as product_category','sp.name as status', 'rs.date as finish_date', 'r.approved_nominal as approved_limit', 'ud.name as marketing_name')
+                        ->select(DB::raw('r.id as id, DATE_FORMAT(r.offering_date,"%Y-%m-%d") as offering_date, r.cust_name,pc.name as product_category,sp.name as status, DATE_FORMAT(rs.date,"%Y-%m-%d") as finish_date, r.approved_nominal as approved_limit, ud.name as marketing_name'))
                         ->get();
         $this->row = 2;
         foreach ($report as &$rep) {
             $id = $rep->id;
             $rep->id = $this->generateRandomAlphanumericString(5,  $id);
             $tempName = $rep->marketing_name;
-            $rep->marketing_name = $rep->approved_limit / 10000000;
+            $rep->marketing_name = number_format($rep->approved_limit / 10000000);
             $rep->point = $tempName;
             $rep->approved_limit = number_format($rep->approved_limit);
             $this->row = $this->row + 1;
+            $rep->offering_date = Date::dateTimeToExcel(new DateTimeImmutable($rep->offering_date));
+            $rep->finish_date = Date::dateTimeToExcel(new DateTimeImmutable($rep->finish_date));
         }
         return $report;
     }
